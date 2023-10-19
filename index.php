61ddb8fd44f41d4e7dda6b1dfbb924883b7847a9
ToddBenrud1 <8894475-ToddBenrud1@users.noreply.replit.com>
2023-10-19 19:28:44 +0000

    2nd change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <?php echo '<p>Hello World</p>'; ?> 
-Initial Setup
+Initial Setup+1
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
   </body>
 </html>
