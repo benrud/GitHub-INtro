28a54c5fc5050134ba3046ab795af3ad7ac71a6b
benrud <benrud@users.noreply.github.com>
2023-10-24 18:19:31 +0000

    10-24V2

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <?php echo '<p>Hello World</p>'; ?> 
-Initial Setup+10-24
+Initial Setup+10-24 We are eating
     
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
   </body>
